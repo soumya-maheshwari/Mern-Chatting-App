63af1afc6becc45aff0494a095a4fe4f5611a4cd
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2024-01-14 11:09:44 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const connectDB = require("./connectDB");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const { errorMiddleware } = require("./middleware/errorHandler");
+
+app.use(express.json());
+app.use(cors({ origin: true }));
+app.use(express.urlencoded({ extended: false }));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.use(errorMiddleware);
+
+// Connection to DataBase
+connectDB();
+
+// routes
