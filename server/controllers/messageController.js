ac057ebe6cd6220508bdab9d45376027ec0e3f21
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2024-01-22 12:04:34 +0530

    image and video upload in chat

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -2,6 +2,7 @@ const Message = require("../models/messageModel");
 const Chat = require("../models/chatModel");
 const User = require("../models/userModel");
 const { ErrorHandler } = require("../middleware/errorHandler");
+const cloudinary = require("cloudinary").v2;
 
 const fetchAllMessages = async (req, res, next) => {
   try {
@@ -23,9 +24,32 @@ const fetchAllMessages = async (req, res, next) => {
 
 const sendMessage = async (req, res, next) => {
   const { content, chatId } = req.body;
-  console.log(chatId, "chat id");
-  if (!content) {
+  // console.log(chatId, "chat id");
+  if (content) {
     next(new ErrorHandler(400, "Enter some text to send"));
+  }
+  // console.log(req.user);
+  let file = req.files ? req.files.file : null;
+  let image = null;
+
+  let video = null;
+
+  console.log(file);
+
+  if (file) {
+    const result = await cloudinary.uploader.upload(file.tempFilePath, {
+      public_id: `${Date.now()}`,
+      resource_type: "auto",
+      folder: "media",
+    });
+
+    if (result.resource_type == "image") {
+      image = result.secure_url;
+    }
+    console.log(image);
+    if (result.resource_type == "video") {
+      video = result.secure_url;
+    }
   }
   if (!chatId) {
     next(new ErrorHandler(400, "invalid data passed into request"));
@@ -34,6 +58,8 @@ const sendMessage = async (req, res, next) => {
       sender: req.user._id,
       content: content,
       chat: chatId,
+      image: image,
+      video: video,
     };
     console.log(newMessage, "new messssageee");
     try {
